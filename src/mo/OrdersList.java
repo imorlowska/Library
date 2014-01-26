2f7c9637fecc4e5999913245cf80988c848496e1
Izabela Orlowska <imorlowska@gmail.com>
2014-01-26 22:39:04 +0100

    implemented creating orders

--- src/mo/OrdersList.java
+++ src/mo/OrdersList.java
@@ -60,6 +60,32 @@ public class OrdersList {
         }
         return found;
     }
+    
+    public void addOrder(int item_id, int client_id, String lend_date, Connect connect) 
+            throws SQLException, InterruptedException {
+        Order o = new Order();
+        o.id = -1;
+        o.item_id = item_id;
+        o.client_id = client_id;
+        o.lend_date = lend_date;
+        o.return_date = null;
+        
+        connect.executeInsert("insert into orders(item_id, client_id, lend_date) values (" + 
+                "'" + item_id + "', '" + client_id + "', '" + lend_date + "');");
+         ResultSet rs = connect.executeQuery("select id, item_id, client_id, lend_date from orders;");
+        while(rs.next()) {
+            if (rs.getInt(2) == item_id && rs.getInt(3) == client_id && rs.getString(4).equals(lend_date)) {
+                o.id = rs.getInt(1);
+                orders.add(o);
+                System.out.println("Successfully added new order, id = " + o.id);
+                break;
+            }
+        }
+        if (o.id == -1) {
+            System.out.println("Failed to add new order.");
+        }
+    }
+    
 
     public void load(Connect connect) throws SQLException {
         orders.clear();
